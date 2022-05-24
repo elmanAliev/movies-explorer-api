67e00462d90fd3391d3123be4915f71a388acf63
Elman Aliev <elman3605@yandex.ru>
2022-05-24 17:37:07 +0300

    feat: реализовать логирование

--- app.js
+++ app.js
@@ -29,33 +29,33 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 // подключаем логгер запросов
 app.use(requestLogger);
 
-// const allowedCors = [
-//   'http://localhost:3000',
-//   'http://localhost:3001',
-//   'http://elman3605.students.nomoredomains.work',
-//   'https://elman3605.students.nomoredomains.work',
-// ];
+const allowedCors = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'http://movies.elman3605.nomoredomains.xyz',
+  'https://movies.elman3605.nomoredomains.xyz',
+];
 
-// app.use((req, res, next) => {
-//   const { origin } = req.headers;
+app.use((req, res, next) => {
+  const { origin } = req.headers;
 
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//     res.header('Access-Control-Allow-Credentials', true);
-//   }
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
 
-//   const { method } = req;
-//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-//   const requestHeaders = req.headers['access-control-request-headers'];
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
 
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
-//     return res.end();
-//   }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
 
-//   return next();
-// });
+  return next();
+});
 
 app.get('/crash-test', () => {
   setTimeout(() => {
