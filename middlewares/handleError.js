38832b4a9324b935eff8be5ccddf484361f0e378
Elman Aliev <elman3605@yandex.ru>
2022-05-28 10:37:13 +0500

    fix: first iteration

--- middlewares/handleError.js
+++ middlewares/handleError.js
@@ -1,8 +1,12 @@
-const handleError = (err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Ошибка сервера';
-  res.status(statusCode).send({ message });
+function handleError(err, req, res, next) {
+  const { message, statusCode = 500 } = err;
+  res
+    .status(statusCode)
+    .send(statusCode === 500
+      ? { message: 'Что-то пошло не так' }
+      : { message });
+
   next();
-};
+}
 
 module.exports = handleError;
